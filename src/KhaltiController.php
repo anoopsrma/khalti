ba0bfcdc54d41ea2ff2566bed104f39caf18dded
anoopsrma <navyanup1@gmail.com>
2018-01-08 20:24:52 +0545

    Khalti

--- src/KhaltiController.php
+++ src/KhaltiController.php
@@ -10,21 +10,24 @@ use DB;
 
 class KhaltiController extends Controller
 {
-    public function showPurchaseForm(){
+    public function showPurchaseForm()
+    {
     	return view('khalti::index');
     }
 
-    public function transactionVerification(Request $request){
+    public function transactionVerification(Request $request)
+    {
     	$data = [
     		'user_id' 	=> $request->input('user_id'),
     		'mobile' 	=> $request->input('mobile'),
     		'amount' 	=> $request->input('amount'),
     		'pre_token' => $request->input('pre_token'),
-    		'created_at'=> Carbon::now();
+    		'created_at'=> Carbon::now()
     	];
 
     	//before verification for reference purposes 
     	$khalti = $this->khalti->create($data);
+    }
 
     public function subtract($a, $b)
     {
